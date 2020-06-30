4f50f167958e72bd89c2ecdae1e5acabd263380b
alishahwee <alisha.hwee@outlook.com>
2020-06-30 13:17:37 -0500

    Set initial state

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -3,6 +3,14 @@ import Display from './Display.js';
 import Buttons from './Buttons.js';
 
 export default class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      output: '',
+    };
+  }
+
   render() {
     return (
       <div className="Calculator">
