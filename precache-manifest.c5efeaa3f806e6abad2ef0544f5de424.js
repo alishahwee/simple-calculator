d5cc2b595b470c5b3fc453b8985ba55f10043f0f
alishahwee <alisha.hwee@outlook.com>
2020-07-07 22:30:24 -0500

    Updates

---
+++ precache-manifest.c5efeaa3f806e6abad2ef0544f5de424.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "755f24cb790a923029b20fa3c2796505",
+    "url": "/simple-calculator/index.html"
+  },
+  {
+    "revision": "4e9a341d7b680b141448",
+    "url": "/simple-calculator/static/css/main.a984512c.chunk.css"
+  },
+  {
+    "revision": "ec7741ac53eb67f0941e",
+    "url": "/simple-calculator/static/js/2.7c96ed72.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/simple-calculator/static/js/2.7c96ed72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e9a341d7b680b141448",
+    "url": "/simple-calculator/static/js/main.3cfd0fec.chunk.js"
+  },
+  {
+    "revision": "908228e8dc8b6d038c48",
+    "url": "/simple-calculator/static/js/runtime-main.68cd5bf8.js"
+  }
+]);
